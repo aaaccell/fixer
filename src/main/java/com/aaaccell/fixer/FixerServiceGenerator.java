4ba5386d05ab707452540b1b4e032065e199b929
Marc Juchli <mail@marcjuch.li>
2020-07-23 14:02:17 +0200

    Fix logging-interceptor compatibility

--- src/main/java/com/aaaccell/fixer/FixerServiceGenerator.java
+++ src/main/java/com/aaaccell/fixer/FixerServiceGenerator.java
@@ -10,28 +10,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class FixerServiceGenerator {
     private static final String BASE_URL = "https://data.fixer.io/api/";
 
-    private static Gson gson = new GsonBuilder()
+    private static final Gson gson = new GsonBuilder()
             .registerTypeAdapterFactory(new FixerResponseTypeAdapterFactory())
             .create();
 
-    private static Retrofit.Builder builder
+    private static final Retrofit.Builder builder
             = new Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create(gson));
 
     private static Retrofit retrofit = builder.build();
 
-    private static OkHttpClient.Builder httpClient
+    private static final OkHttpClient.Builder httpClient
             = new OkHttpClient.Builder();
 
-    private static HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+    private static final HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
 
     public static <S> S createService(Class<S> serviceClass) {
         return createService(serviceClass, HttpLoggingInterceptor.Level.BASIC);
     }
 
     public static <S> S createService(Class<S> serviceClass, HttpLoggingInterceptor.Level logLevel) {
-        logging.level(logLevel);
+        logging.setLevel(logLevel);
         if (!httpClient.interceptors().contains(logging)) {
             httpClient.addInterceptor(logging);
             builder.client(httpClient.build());
